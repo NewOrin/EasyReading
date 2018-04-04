72f709a2f8649f13dde6bf636c960ab54fb782fd
zhangfubin <zhangfubin@91zdan.com>
2018-04-04 11:46:41 +0800

    完善布局，删除多余导入包

--- app/src/main/java/com/neworin/easyreading/ui/home/HomeActivity.kt
+++ app/src/main/java/com/neworin/easyreading/ui/home/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.neworin.easyreading.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import com.neworin.easyreading.R
 import com.neworin.easyreading.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_home.*
@@ -12,6 +14,9 @@ class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        home_tb.title = "ReadHub"
+        home_tb.setTitleTextColor(Color.WHITE)
+        home_tb.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent))
         mTitles.add("热门话题")
         home_viewpager.adapter = HomeAdapter(supportFragmentManager, mTitles)
         home_tab_layout.setupWithViewPager(home_viewpager)
